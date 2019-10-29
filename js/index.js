4a645c5c79993681cf6a3979dc605352fed3e4a8
Rafal Kaniewski <rafal.kaniewski@gmail.com>
2019-10-29 14:34:55 +0100

    Added event and prevent for navbar

--- js/index.js
+++ js/index.js
@@ -3,3 +3,11 @@ const logo = document.querySelector('.logo-heading');
 logo.addEventListener('click', function(e) {
   e.target.style.color = 'red';
 });
+
+const navMenu = document.querySelectorAll('.nav-link');
+navMenu.forEach(function(cV) {
+  cV.addEventListener('click', function(e) {
+    cV.style.color = 'red';
+    e.preventDefault();
+  });
+});
